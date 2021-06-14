339743d38c00766311f8a0d8c3aaf7d2bba45ee4
taufiqm <fiqmurtadho@gmail.com>
2021-06-14 14:09:05 +0900

    method equivalence unittest

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,1 +1,19 @@
+import unittest
+import numpy as np
+import sys
 
+sys.path.append('../')
+
+from src import quantumxy as qxy
+
+
+class TestHamiltonian(unittest.TestCase):
+
+    def test_MethodEquivalence(self):
+        booleanList = [np.array_equal(qxy.method1_hamiltonian(2,1,i), qxy.method2_hamiltonian(2,1,i)) for i in range(2,11)]
+        print(booleanList)
+        self.assertTrue(all(booleanList))
+
+
+if __name__ == '__main__':
+    unittest.main()
