49262eb69f0c31746ddb03a2745be9ae8db4b120
taufiqm <fiqmurtadho@gmail.com>
2021-06-14 15:08:45 +0900

    debug, change h to -h

--- src/quantumxy.py
+++ src/quantumxy.py
@@ -14,14 +14,14 @@ def method1_hamiltonian(h,J,N):
         if sequence[N-1]!=sequence[0]:                                  #periodic boundary condition
             newseq = sequence.copy()
             newseq[N-1] = sequence[0]; newseq[0] = sequence[N-1]
-            mat[bitsequence.index(sequence), bitsequence.index(newseq)]+= -1
+            mat[bitsequence.index(sequence), bitsequence.index(newseq)]+= -J
         for i in range(N-1):
             if sequence[i]!=sequence[i+1]:
                 newseq = sequence.copy()
                 newseq[i] = sequence[i+1]; newseq[i+1]=sequence[i]
-                mat[bitsequence.index(sequence), bitsequence.index(newseq)]+=-1
+                mat[bitsequence.index(sequence), bitsequence.index(newseq)]+=-J
     for i in range(2**N):
-        mat[i,i] = h*sum(bitsequence[i])-h*(N - sum(bitsequence[i]))
+        mat[i,i] = -h*sum(bitsequence[i])+h*(N - sum(bitsequence[i]))
     return mat
 
 #Constructing the Hamiltonian matrix by kronecker product
@@ -32,15 +32,15 @@ def method2_hamiltonian(h,J,N):
     mat = np.zeros((2**N, 2**N))
     for i in range(N):
         if i == 0:
-            hopping = -np.kron(np.kron(pauli_plus, pauli_minus),np.identity(2**(N-2)))
-            mat+= h*np.kron(pauliz, np.identity(2**(N-1)))
+            hopping = -J*np.kron(np.kron(pauli_plus, pauli_minus),np.identity(2**(N-2)))
+            mat+= -h*np.kron(pauliz, np.identity(2**(N-1)))
             mat+=hopping+np.conj(hopping.T)
         elif i == N-1:                                                      #periodic boundary condition
-            hopping = -np.kron(np.kron(pauli_minus, np.identity(2**(N-2))), pauli_plus)
-            mat+= h*np.kron(np.identity(2**(N-1)), pauliz)
+            hopping = -J*np.kron(np.kron(pauli_minus, np.identity(2**(N-2))), pauli_plus)
+            mat+= -h*np.kron(np.identity(2**(N-1)), pauliz)
             mat+= hopping+np.conj(hopping.T)
         else:
-            hopping = -np.kron(np.kron(np.identity(2**i), np.kron(pauli_plus, pauli_minus)), np.identity(2**(N-i-2)))
-            mat+= h*np.kron(np.kron(np.identity(2**i),pauliz), np.identity(2**(N-i-1)))
+            hopping = -J*np.kron(np.kron(np.identity(2**i), np.kron(pauli_plus, pauli_minus)), np.identity(2**(N-i-2)))
+            mat+= -h*np.kron(np.kron(np.identity(2**i),pauliz), np.identity(2**(N-i-1)))
             mat+=hopping+np.conj(hopping.T)
     return mat 
